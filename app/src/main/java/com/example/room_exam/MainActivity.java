f6875ded4c4edf4106ffc7850bbf58546bad0107
mjcho1 <alswlwh1224@naver.com>
2020-07-23 19:15:25 +0900

    LiveData Test

--- app/src/main/java/com/example/room_exam/MainActivity.java
+++ app/src/main/java/com/example/room_exam/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.room_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +28,14 @@ public class MainActivity extends AppCompatActivity {
                 .allowMainThreadQueries()
                 .build();
 
-        mResultTextView.setText(db.todoDao().getAll().toString());
+        // UI 갱신 -> LiveData
+        db.todoDao().getAll().observe(this, todos -> {
+            mResultTextView.setText(todos.toString());
+        });
 
-        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-                mResultTextView.setText(db.todoDao().getAll().toString());
-            }
+        // 버튼 클릭 시 DB 에 insert
+        findViewById(R.id.add_button).setOnClickListener(v -> {
+            db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
         });
     }
 }
